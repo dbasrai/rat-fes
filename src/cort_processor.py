c0306e3fce1a85c0702ba6c3e19f8aa80282cf85
dbasrai <diyabasrai@gmail.com>
2022-04-27 08:29:29 -0500

    .

--- src/cort_processor.py
+++ src/cort_processor.py
@@ -1,1 +1,11 @@
-#love
+from src.neural_analysis import *
+from src.wiener_filter import *
+from src.folder_handler import *
+
+class CortProcessor:
+    def __init__(self, folder_path):
+        self.handler = FolderHandler(folder_path)
+
+   # def process
+   # def decode
+   
