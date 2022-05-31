06668df761d98ba9423a918f0a9ea29d1aa68842
dbasrai <diyabasrai@gmail.com>
2022-05-31 08:06:50 -0500

    merge

--- scripts/sylink_vids.py
+++ scripts/sylink_vids.py
@@ -1,17 +1,28 @@
 import os
+import argparse
 
-def doit(folder):
+def parse_args():
+    parser = argparse.ArgumentParser(description='pass in folder')
+    parser.add_argument('echo')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
     substring = 'live_videos'
     vid_name_list = []
     vid_path_list= []
-    for root, dirs, files in os.walk(folder):
+    for root, dirs, files in os.walk(args.echo):
         if substring in root:
             for filename in files:
                 if filename.endswith('.avi'):
+                    print(filename)
                     vid_name_list.append(f'videos/{filename}')
                     vid_path_list.append(f'{root}/{filename}')
-    os.mkdir('videos')
+    os.mkdir('./videos')
 
     for i in range(len(vid_name_list)):
         os.symlink(vid_path_list[i], vid_name_list[i])
 
+if __name__ == '__main__':
+    main()
---
+++ src/dummy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def big_dummy():
+    x = np.arange(0,30)
+    x = np.power(x, 3)
+    return x
