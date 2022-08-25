ead11fd5f657582e83e6267f02e918675e18429a
dbasrai <diyabasrai@gmail.com>
2022-08-25 01:34:10 -0500

    1:33am, weirdly enough things don't quite work

--- src/cca_processor.py
+++ src/cca_processor.py
@@ -8,10 +8,10 @@ from src.decoders import *
 class CCAProcessor:
     def __init__(self, cp1, cp2, limbfoot_angle=1):
         self.cp1 = cp1
-        self.cp1.decode_angles()
+        self.cp1.h, self.cp1.vaf, nada, nada = self.cp1.decode_angles()
         self.cp1.get_gait_indices()
         self.cp2 = cp2
-        self.cp2.decode_angles()
+        self.cp2.h, self.cp2.vaf, nada, nada = self.cp2.decode_angles()
         self.cp2.get_gait_indices()
         self.limbfoot_angle=limbfoot_angle
 
@@ -26,7 +26,29 @@ class CCAProcessor:
             return output
 
     def process_and_align_kinematics(self):
+        avg_samples = self.cp1.avg_gait_samples #arbitrarily grab from 1
         cp1_gait_x, cp1_gait_y = self.cp1.remove_bad_gaits()
-        cp2_gait_x, cp2_gait_y = self.cp2.remove_bad_gaits()
-        return cp1_gait_x, cp2_gait_x
-        
+        cp2_gait_x, cp2_gait_y = self.cp2.remove_bad_gaits(avg_gait_samples =
+                avg_samples)
+
+        if len(cp1_gait_x) >= len(cp2_gait_x):
+            end_slice = len(cp2_gait_x)
+            cp1_gait_x = cp1_gait_x[:end_slice]
+            cp1_gait_y = cp1_gait_y[:end_slice]
+        else:
+            end_slice = len(cp1_gait_x)
+            cp2_gait_x = cp2_gait_x[:end_slice]
+            cp2_gait_y = cp2_gait_y[:end_slice]
+
+        total_samples = cp1_gait_x.shape[0] * cp1_gait_x.shape[1]
+
+        cp1_gait_x = np.reshape(cp1_gait_x, (total_samples,
+            cp1_gait_x.shape[2]))
+        cp1_gait_y = np.reshape(cp1_gait_y, (total_samples,
+            cp1_gait_y.shape[2]))
+        cp2_gait_x = np.reshape(cp2_gait_x, (total_samples, 
+            cp2_gait_x.shape[2]))
+        cp2_gait_y = np.reshape(cp2_gait_y, (total_samples,
+            cp2_gait_y.shape[2]))
+
+        return cp1_gait_x, cp1_gait_y, cp2_gait_x, cp2_gait_y 
